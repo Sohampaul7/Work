a8a4e521bc0da94487f58fad4496f699dda5d0cb
Sohampaul7 <paulsoham63@gmail.com>
2024-12-19 18:22:43 +0100

    [EDIT] Only setup

--- TOF_minimal_example/TOF_minimal_example.ino
+++ TOF_minimal_example/TOF_minimal_example.ino
@@ -1,6 +1,8 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);       // Debugging output to Serial Monitor
+  Serial2.begin(115200);    // TFMini-S is connected to Serial2 (pins 7 and 8)
+  
+  Serial.println("System Initialized. Starting the loop.");
 }
 
 void loop() {
