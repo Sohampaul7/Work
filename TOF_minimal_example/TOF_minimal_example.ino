cd8fe672e6ad38968b70a258f318ebea3aa2cde7
Sohampaul7 <paulsoham63@gmail.com>
2024-12-19 19:11:12 +0100

    [ADD] reading the data

--- TOF_minimal_example/TOF_minimal_example.ino
+++ TOF_minimal_example/TOF_minimal_example.ino
@@ -1,4 +1,13 @@
 
+#define HEADER 0x59   // Frame starting byte (0x59 for TFMini-S)
+#define BUFFER_SIZE 9 // Size of the data packet
+
+// Variables for ToF sensor
+uint8_t uart[BUFFER_SIZE];
+int16_t dist;
+uint8_t chk;
+int i;
+
 void setup() {
   Serial.begin(9600);       // Debugging output to Serial Monitor
   Serial2.begin(115200);    // TFMini-S is connected to Serial2 (pins 7 and 8)
@@ -7,9 +16,30 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("in loop");
-  if (Serial2.available()){
-    Serial.println("Data available");
+  // Read distance data from the sensor
+  if (Serial2.available()) {
+    if (Serial2.read() == HEADER) {
+      uart[0] = HEADER;
+
+      if (Serial2.read() == HEADER) {
+        uart[1] = HEADER;
+
+        for (i = 2; i < BUFFER_SIZE; i++) {
+          uart[i] = Serial2.read();
+        }
+
+        // Calculate chksum
+        chk = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
+
+        if (uart[8] == (chk & 0xFF)) {
+          // Calculate distance
+          dist = uart[2] + uart[3] * 256;
+        }
+      }
+    }
   }
-  delay(500);
+
+  Serial.println(dist);
+
+  delay(5);
 }
