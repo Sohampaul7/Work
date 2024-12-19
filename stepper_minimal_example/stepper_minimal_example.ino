d46a7aa75ad70939e59fac04b9c17192fcc79959
Sohampaul7 <paulsoham63@gmail.com>
2024-12-19 19:34:31 +0100

    [ADD] issues- vibration, angle not 180

--- stepper_minimal_example/stepper_minimal_example.ino
+++ stepper_minimal_example/stepper_minimal_example.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#define STEP_PIN 3    // Pin connected to stepper motor step input
+#define DIR_PIN 4     // Pin connected to stepper motor direction input
 
+
+const float stepPerDegree = 200.0/360.0;
+const float requiredAngle = 180.0;
+
+void setup() {
+  pinMode(STEP_PIN, OUTPUT); // Stepper motor step pin
+  pinMode(DIR_PIN, OUTPUT);  // Stepper motor direction pin
+
+  Serial.println("Setup complete");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(DIR_PIN, HIGH); 
+  
+  for (int stp = 0; stp < requiredAngle*stepPerDegree; stp++) {
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(3500); // Adjust speed
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(3500);
+  }
+
+  digitalWrite(DIR_PIN, LOW); 
+  
+  for (int stp = 0; stp < requiredAngle*stepPerDegree; stp++) {
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(3500); // Adjust speed
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(3500);
+  }
+
+  
 
 }
